bdacbaef723069d96d233eeca12db1dc8380af08
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-04-04 16:54:00 +0300

    palindrome lab

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word){
+  return word.split("").reverse().join("")
 }
+
+
+function isPalindrome(word) {
+const reversedWord=reverse(word)
+return word === reversedWord
+   
+    }
+  
+
 
 /* 
   Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+ 
 */
 
 // You can run `node index.js` to view these console logs
